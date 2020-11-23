96897d15e2569da617fee30ac1e0b95ce86ba2b6
tldms <tldms201@naver.com>
2020-11-23 23:52:25 +0900

    install flask and make local DE and url branch

---
+++ week04/app.py
@@ -0,0 +1,18 @@
+# flask 설치하기
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return '<button>버튼</button>'
+
+
+@app.route('/mypage')
+def mypage():
+    return 'This is mypage!'
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
